b7062562c0084edc31df1267043f8d9cb8f08e68
Aditya <64887773+AdityaCoder7@users.noreply.github.com>
2023-12-04 21:27:15 +0530

    portfolio

---
+++ src/components/About/About.tsx
@@ -0,0 +1,104 @@
+import { Container } from "./styles";
+import AdityaSingh from "../../assets/AdityaSingh.jpg";
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+
+export function About() {
+  return (
+    <Container id="about">
+      <div className="about-text">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h2>About me</h2>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+          Hi there! I'm Aditya, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, and React, I have the tools to bring any website vision to life.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          <p>
+          An avid learner with a knack of problem-solving as well. My career objective is to secure a role in the IT workspace that allows me to continuously enhance my skills while contributing to the growth and success of the organization.
+On this path, I intend to be staying toe-to-toe with the upcoming technologies and innovations in the IT landscape.</p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
+          <p>
+          Passionate web developer with a flair for creativity and a problem-solving mindset. Specializing in crafting visually stunning websites for clients while seamlessly navigating through challenges to deliver innovative solutions.</p>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
+          <h3>Here are my main skills:</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+              <img src={wordpress} alt="Wordpress" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={shopify} alt="shopify" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={typescriptIcon} alt="Typescript" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
+              <img src={vueIcon} alt="Vue" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={boostrapIcon} alt="bootstrap" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
+          </div>
+        </div>
+      </div>
+      <div className="about-image">
+        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
+          <img src={AdityaSingh} alt="Aditya Singh" />
+        </ScrollAnimation>
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,65 @@
+import { Container } from './styles'
+
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+// import discordIcon from '../../assets/discord.png'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      {/*<a href="https://Adityasingh.in" className="logo">
+        <span>www.Aditya</span>
+        <span>singh.in</span>
+  </a>*/}
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/aditya-singh-648406230"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/Aditya7781/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://api.whatsapp.com/send/?phone=%2B918305975275&text=Hello+Aditya"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={whatsapp} alt="Whatsapp" />
+        </a>
+        <a
+          href="https://t.me/ElonMast69"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/aditya._singh07/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+     
+      </div>
+    </Container>
+  )
+}
